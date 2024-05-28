2049546e725cb57a399f9697733e0a8f02773cda
Mai Van Nhan <gplgarena@gmail.com>
2024-05-28 10:09:40 +0700

    send data from html

--- src/main/java/vn/laptop/laptopshop/controller/UserController.java
+++ src/main/java/vn/laptop/laptopshop/controller/UserController.java
@@ -1,11 +1,13 @@
 package vn.laptop.laptopshop.controller;
 
+import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.laptop.laptopshop.domain.User;
 import vn.laptop.laptopshop.service.UserService;
 
 @Controller
@@ -20,17 +22,28 @@ public class UserController {
     @RequestMapping("/")
     public String getHomePage(Model model) {
         String test = this.userService.handleHello();
-        model.addAttribute("eric", test);
+        model.addAttribute("user", test);
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User nhan) {
+        System.out.println("run time" + nhan);
         return "hello";
     }
 
 }
 
-// dependency injection
-
 // @RestController
 // public class UserController {
 
+// // DI : dependency injection
 // private UserService userService;
 
 // public UserController(UserService userService) {
@@ -41,5 +54,4 @@ public class UserController {
 // public String getHomePage() {
 // return this.userService.handleHello();
 // }
-
 // }
